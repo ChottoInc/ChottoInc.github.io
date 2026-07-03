b41d2624a11cc6c15aaba275dc883184c1f76eea
Troilo Matteo <168276638+ChottoInc@users.noreply.github.com>
2026-07-03 09:42:19 +0200

    Delete script.js

--- script.js
+++
@@ -1,4 +0,0 @@
-document.getElementById('theme-toggle').addEventListener('click', function () {
-    document.body.classList.toggle('dark-theme');
-    this.textContent = document.body.classList.contains('dark-theme') ? '☀️' : '🌙';
-});
